c04d0e9343fd2d10da0da845ddbee374f12662be
BenjaminAedo <benjamin.aedo.ch@gmail.com>
2023-11-18 01:19:01 -0300

    app tarea

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppFoto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
